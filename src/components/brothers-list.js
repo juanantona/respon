5d2ccce77b9b37c468b9b88d2afdeccb8f330a61
Juanan Antona <juan.antonadelval@gmail.com>
2020-05-13 11:14:07 +0200

    Get items from db through the backend

--- src/components/brothers-list.js
+++ src/components/brothers-list.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -9,23 +9,38 @@ import Avatar from '@material-ui/core/Avatar';
 import PersonIcon from '@material-ui/icons/Person';
 import Checkbox from '@material-ui/core/Checkbox';
 
-const brothers = [
-  { nickName: 'Pepe' },
-  { nickName: 'Juan' },
-  { nickName: 'María' }
-];
+const apiEndpoint = 'http://localhost:3000/brothers';
 
 export default function BrothersList() {
+  const [brothers, updateBrothers] = useState([]);
+
+  useEffect(() => {
+    try {
+      fetch(apiEndpoint, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          updateBrothers(data);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
   return (
     <List>
-      {brothers.map(brother => (
-        <ListItem button>
+      {brothers.map((brother, index) => (
+        <ListItem key={index} button>
           <ListItemAvatar>
             <Avatar>
               <PersonIcon />
             </Avatar>
           </ListItemAvatar>
-          <ListItemText primary={brother.nickName} />
+          <ListItemText primary={brother.nick_name} />
           <ListItemSecondaryAction>
             <IconButton edge="end" aria-label="delete">
               <Checkbox />
