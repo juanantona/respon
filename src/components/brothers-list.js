66c266c4da03c616be65bea4ac126992e7296cba
Juanan Antona <juan.antonadelval@gmail.com>
2020-05-19 19:55:57 +0200

    Getting better brothers list styling

--- src/components/brothers-list.js
+++ src/components/brothers-list.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { styled } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -7,28 +8,46 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import IconButton from '@material-ui/core/IconButton';
 import Avatar from '@material-ui/core/Avatar';
 import PersonIcon from '@material-ui/icons/Person';
-import Checkbox from '@material-ui/core/Checkbox';
+import EditIcon from '@material-ui/icons/Edit';
+
+const ListWrapper = styled(List)({
+  '& li': {
+    border: '#0000000a solid 1px',
+    borderRadius: '5px',
+    marginBottom: '5px',
+    '&:hover': {
+      border: '#0000001a solid 1px',
+      '& .brother-controls': {
+        display: 'inline'
+      }
+    }
+  }
+});
+
+const ControlsWrapper = styled(ListItemSecondaryAction)({
+  display: 'none'
+});
 
 export default function BrothersList(props) {
   const { brothers } = props;
 
   return (
-    <List>
+    <ListWrapper>
       {brothers.map((brother, index) => (
-        <ListItem key={index} button>
+        <ListItem key={index}>
           <ListItemAvatar>
             <Avatar>
               <PersonIcon />
             </Avatar>
           </ListItemAvatar>
           <ListItemText primary={brother.nick_name} />
-          <ListItemSecondaryAction>
-            <IconButton edge="end" aria-label="delete">
-              <Checkbox />
+          <ControlsWrapper className="brother-controls">
+            <IconButton edge="end" aria-label="edit">
+              <EditIcon />
             </IconButton>
-          </ListItemSecondaryAction>
+          </ControlsWrapper>
         </ListItem>
       ))}
-    </List>
+    </ListWrapper>
   );
 }
